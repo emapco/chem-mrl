7af03ba05e663cfaf1e2a2155901741456896555
Manny Cortes <polosetroc@gmail.com>
2025-01-30 18:14:04 -0800

    refactor: include sentence transformers MRL loss functions to address bug in current library implementation - will remove once PR (https://github.com/UKPLab/sentence-transformers/pull/3203) is accept and change reflects the pip package.

--- chem_mrl/losses/__init__.py
+++ chem_mrl/losses/__init__.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 from .ClassifierLoss import SelfAdjDiceLoss, SoftmaxLoss
+from .Matryoshka2dLoss import Matryoshka2dLoss
+from .MatryoshkaLoss import MatryoshkaLoss
 from .TanimotoLoss import TanimotoSentLoss, TanimotoSimilarityLoss
 
 __all__ = [
+    "MatryoshkaLoss",
+    "Matryoshka2dLoss",
     "SelfAdjDiceLoss",
     "SoftmaxLoss",
     "TanimotoSentLoss",
