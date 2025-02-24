25e85e2c5eb5395e30edd657620b40e1a168a426
Manny Cortes <polosetroc@gmail.com>
2025-02-23 22:07:58 -0800

    add: dataset generation/augmentation scripts

--- dataset/process_all_smiles_datasets.py
+++ dataset/process_all_smiles_datasets.py
@@ -1,35 +1,16 @@
-import logging
+import importlib.util
 import os
-import sys
 
-# import pandas as cudf  # cpu
-import cudf  # gpu
 from pandarallel import pandarallel
 from pandas import DataFrame
-from rdkit import RDLogger
+from util import init_logging, parallel_canonicalize_smiles
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler(sys.stdout)],
-)
-RDLogger.DisableLog("rdApp.*")  # type: ignore - DisableLog is an exported function
+if importlib.util.find_spec("cudf") is not None:
+    import cudf  # type: ignore - gpu
+else:
+    import pandas as cudf
 
-
-def get_canonical_smiles(smiles: str) -> str | None:
-    from rdkit.Chem import MolFromSmiles, MolToSmiles
-    from rdkit.Chem.MolStandardize.rdMolStandardize import StandardizeSmiles
-
-    mol = MolFromSmiles(smiles)
-    if mol is None:
-        try:
-            mol = MolFromSmiles(StandardizeSmiles(smiles))  # may raise error
-            if mol is None:
-                return None
-        except Exception:
-            return None
-    smiles = MolToSmiles(mol, canonical=True)
-    return smiles
+logger = init_logging(__name__)
 
 
 def load_parquet_file(file_path: str) -> cudf.DataFrame:
@@ -92,32 +73,28 @@ def clean_dataframe(df: cudf.DataFrame) -> DataFrame:
 
     # Convert to pandas dataframe if using cudf library
     # Need to apply function on strings - not currently supported by cudf
-    if hasattr(df, "to_pandas"):
-        if callable(df.to_pandas):
-            pd_df = df.to_pandas()
-    assert isinstance(pd_df, DataFrame)
-    del df
+    if hasattr(df, "to_pandas") and callable(df.to_pandas):
+        df = df.to_pandas()
+    assert isinstance(df, DataFrame)
 
     # Use pandarallel to parallelize get_canonical_smiles and then remove invalid rows
     pandarallel.initialize(progress_bar=True)
-    logging.info("Canonicalizing SMILES strings")
-    pd_df["canonical_smiles"] = pd_df["smiles"].parallel_apply(get_canonical_smiles)
-    pd_df.dropna(subset=["canonical_smiles"], inplace=True)
-    pd_df.drop_duplicates(
-        subset=["canonical_smiles"], keep="first", inplace=True, ignore_index=True
-    )
-    return pd_df
+    logger.info("Canonicalizing SMILES strings")
+    df["canonical_smiles"] = df["smiles"].parallel_apply(parallel_canonicalize_smiles)
+    df.dropna(subset=["canonical_smiles"], inplace=True)
+    df.drop_duplicates(subset=["canonical_smiles"], keep="first", inplace=True, ignore_index=True)
+    return df
 
 
 def process_all_chemistry_datasets(output_dir: str) -> DataFrame:
     dfs = []
     valid_files = get_valid_files(output_dir)
 
-    for file in valid_files:
-        logging.info(f"Processing {file}")
+    for file in valid_files[:1]:
+        logger.info(f"Processing {file}")
         df = load_parquet_file(os.path.join(output_dir, file))
         dfs.append(df)
-        logging.info(f"Loaded df size: {len(df)}")
+        logger.info(f"Loaded df size: {len(df)}")
 
     combined_df = cudf.concat(dfs, ignore_index=True)
     del dfs
@@ -128,14 +105,15 @@ def process_all_chemistry_datasets(output_dir: str) -> DataFrame:
 def main():
     curr_file_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(curr_file_dir)
-    processed_output_dir = os.path.join(parent_dir, "data", "chem")
-    preprocessed_output_dir = os.path.join(processed_output_dir, "preprocessed")
+    data_dir = os.path.join(parent_dir, "data")
+    processed_output_dir = os.path.join(data_dir, "processed")
+    preprocessed_output_dir = os.path.join(data_dir, "preprocessed")
 
     df = process_all_chemistry_datasets(preprocessed_output_dir)
-    logging.info(f"Final df size: {len(df)}")
+    logger.info(f"Final df size: {len(df)}")
 
     output_path = os.path.join(processed_output_dir, "full_ds_canonical.parquet")
-    logging.info(f"Saving dataset to {output_path}")
+    logger.info(f"Saving dataset to {output_path}")
     df.to_parquet(
         output_path,
         engine="fastparquet",
